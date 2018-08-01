c0cd69d4fb2f7074f753921573e8df52e5f74a66
Larry <xiaozi7912@gmail.com>
2018-08-01 10:14:57 +0800

    [Title] Modified [Link] [Symptom] NA [Root Cause] NA [Solution] NA

--- app/src/main/java/com/xiaozi/android/qrcode/scanner/ScannerActivity.java
+++ app/src/main/java/com/xiaozi/android/qrcode/scanner/ScannerActivity.java
@@ -2,13 +2,15 @@ package com.xiaozi.android.qrcode.scanner;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.util.Base64;
 
 import com.google.zxing.Result;
 import com.xiaozi.android.qrcode.scanner.utils.Compressor;
 import com.xiaozi.framework.libs.BaseActivity;
 import com.xiaozi.framework.libs.utils.Logger;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -54,14 +56,26 @@ public class ScannerActivity extends BaseActivity implements ZXingScannerView.Re
     public void handleResult(Result result) {
         Logger.i(LOG_TAG, "handleResult");
         Logger.d(LOG_TAG, "handleResult result.getText : " + result.getText());
+        Logger.d(LOG_TAG, "handleResult result.getText.length : " + result.getText().length());
         String uncompressedString = null;
+        String base64String = null;
+        JSONObject rootObject = null;
+
         try {
+            rootObject = new JSONObject(result.getText());
+            base64String = rootObject.getString("data");
+            Logger.d(LOG_TAG, "handleResult base64String : " + base64String);
+            Logger.d(LOG_TAG, "handleResult base64String decode : " + new String(Base64.decode(base64String, Base64.NO_WRAP)));
+
             uncompressedString = Compressor.uncompress(result.getText());
             Logger.d(LOG_TAG, "handleResult uncompressedString : " + uncompressedString);
+            Logger.d(LOG_TAG, "handleResult uncompressedString.length : " + uncompressedString.length());
             Intent intent = new Intent(mActivity, ScanResultActivity.class);
             intent.putExtra("scan_result", uncompressedString);
             startActivity(intent);
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
             e.printStackTrace();
         }
         mScannerView.resumeCameraPreview(this);
